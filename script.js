fd8145392d649679b73b34469e86d26eddf4c6b8
MindistCalm <51086034+MIndistCalm@users.noreply.github.com>
2021-09-18 15:53:39 +0700

    added index.html script.js style.css

---
+++ script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'How old is Florin?',
+        a: '10',
+        b: '14',
+        c: '20',
+        d: '26',
+        correct: 'c'
+    },{
+        question: 'What my favorite programming language?',
+        a: 'Java',
+        b: 'C++',
+        c: 'JS',
+        d: 'Python',
+        correct: 'c'
+    },{
+        question: 'Who is the president of USA?',
+        a: 'Florin Pop',
+        b: 'Donald Trump',
+        c: 'Ivan Saldano',
+        d: 'Vladimir Putin',
+        correct: 'b'
+    },{
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Application Terminals',
+        correct: 'a'
+    },{
+        question: 'What year was JS launched?',
+        a: '1996',
+        b: '1997',
+        c: '1998',
+        d: 'none of the above',
+        correct: 'd'
+    },
+]
