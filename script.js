93d2e850c48f271c2796ceb23de1820333c45b58
MindistCalm <51086034+MIndistCalm@users.noreply.github.com>
2021-09-18 18:34:51 +0700

    60%

--- script.js
+++ script.js
@@ -35,4 +35,37 @@ const quizData = [
         d: 'none of the above',
         correct: 'd'
     },
-]
+];
+
+
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+
+
+let currentQuiz = 0;
+
+loadQuiz();
+
+function loadQuiz(){
+    const currentQuizData = quizData[currentQuiz];
+    
+    questionEl.innerHTML = currentQuizData.question;
+    
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+submitBtn.addEventListener('click', () => {
+    currentQuiz++;
+
+    currentQuiz < quizData.length ? loadQuiz() : alert("Congratulation!");
+
+    loadQuiz();
+})
