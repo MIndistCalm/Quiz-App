956e0b5a6d1447a94504a0b9a3888792138e93ea
Mindist <51086034+MIndistCalm@users.noreply.github.com>
2021-10-18 14:53:52 +0700

    100% done

--- script.js
+++ script.js
@@ -1,102 +1,100 @@
 const quizData = [
     {
-        question: 'How old is Florin?',
-        a: '10',
-        b: '14',
-        c: '20',
-        d: '26',
-        correct: 'b'
-    },{
-        question: 'What my favorite programming language?',
-        a: 'Java',
-        b: 'C++',
-        c: 'JS',
-        d: 'Python',
-        correct: 'c'
-    },{
-        question: 'Who is the president of USA?',
-        a: 'Florin Pop',
-        b: 'Donald Trump',
-        c: 'Ivan Saldano',
-        d: 'Vladimir Putin',
-        correct: 'b'
-    },{
-        question: 'What does HTML stand for?',
-        a: 'Hypertext Markup Language',
-        b: 'Cascading Style Sheet',
-        c: 'Jason Object Notation',
-        d: 'Application Terminals',
-        correct: 'a'
-    },{
-        question: 'What year was JS launched?',
-        a: '1996',
-        b: '1997',
-        c: '1998',
-        d: 'none of the above',
-        correct: 'd'
+        question: "What is the most used programming language in 2019?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "JavaScript",
+        correct: "d",
+    },
+    {
+        question: "Who is the President of US?",
+        a: "Florin Pop",
+        b: "Donald Trump",
+        c: "Ivan Saldano",
+        d: "Mihai Andrei",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Cascading Style Sheet",
+        c: "Jason Object Notation",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
     },
 ];
 
-const answersEls = document.querySelectorAll('answer');
-const questionEl = document.getElementById('question');
-
-const a_text = document.getElementById('a_text');
-const b_text = document.getElementById('b_text');
-const c_text = document.getElementById('c_text');
-const d_text = document.getElementById('d_text');
-
-const submitBtn = document.getElementById('submit');
-
-
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
 
 let currentQuiz = 0;
-let answer = undefined;
 let score = 0;
 
 loadQuiz();
 
-function loadQuiz(){
-    deselectedAnswers();
+function loadQuiz() {
+    deselectAnswers();
+
     const currentQuizData = quizData[currentQuiz];
-    
-    questionEl.innerHTML = currentQuizData.question;
-    
+
+    questionEl.innerText = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
     b_text.innerText = currentQuizData.b;
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
 }
 
-function getSelected(){
+function getSelected() {
     let answer = undefined;
-    answersEls.forEach( (answerEl) => {
-        if(answerEl.checked){
-            return answerEl.id;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
         }
     });
+
     return answer;
 }
 
-function deselectedAnswers(){
-    
-        answersEls.forEach( (answerEl) => {
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
         answerEl.checked = false;
     });
 }
 
-submitBtn.addEventListener('click', () => {
-    
-
+submitBtn.addEventListener("click", () => {
+    // check to see the answer
     const answer = getSelected();
 
-    if(answer && answer === quizData[currentQuiz].correct){
-            
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
         currentQuiz++;
-        currentQuiz < quizData.length ? loadQuiz() : alert("Congratulation!");
-        score++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+                
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
     }
-
-
-
-    // loadQuiz();
-})
+});
